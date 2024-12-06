c740bb930d3c9222897506ba0521bc3dd0612e42
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-12-06 21:08:45 +0500

    Task2

---
+++ Tyuiu.OsadetsAA.Sprint6.Task2.V13/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.OsadetsAA.Sprint6.Task2.V13
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
