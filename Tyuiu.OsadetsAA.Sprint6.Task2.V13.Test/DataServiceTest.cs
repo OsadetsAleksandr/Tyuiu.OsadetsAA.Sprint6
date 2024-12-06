ad967a62320343ecfededad9245c753f6c9780d7
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-12-06 21:12:47 +0500

    Task2

--- Tyuiu.OsadetsAA.Sprint6.Task2.V13.Test/DataServiceTest.cs
+++ Tyuiu.OsadetsAA.Sprint6.Task2.V13.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.OsadetsAA.Sprint6.Task2.V13.Test
             int startValue = -5;
             int stopValue = 5;
             var res = ds.GetMassFunction(startValue, stopValue);
-            double[] wait = [3.28, -12.38, -14.03, -5.58, 0.65, 0, -0.65, 5.58, 14.03, 12.38, -3.28];
+            double[] wait = [3.30, -12.37, -14.02, -5.57, 0.65, 0, -0.65, 5.57, 14.02, 12.37, -3.30];
             CollectionAssert.AreEqual(wait, res);
         }
     }
