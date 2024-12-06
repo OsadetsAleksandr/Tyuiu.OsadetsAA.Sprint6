b2b3c05ae78346eba355077cdf98292b5ac27e2d
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-12-06 18:13:28 +0500

    Task0

--- Tyuiu.OsadetsAA.Sprint6.Task0.V27.Lib/DataService.cs
+++ Tyuiu.OsadetsAA.Sprint6.Task0.V27.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.OsadetsAA.Sprint6.Task0.V27.Lib
         public double Calculate(int x)
         {
             double y = 4 * Math.Pow(x, 2) - Math.Pow(x, 3) - (1.5 * x);
-            return Math.Round(y, 3);
+            return y;
         }
     }
 }
