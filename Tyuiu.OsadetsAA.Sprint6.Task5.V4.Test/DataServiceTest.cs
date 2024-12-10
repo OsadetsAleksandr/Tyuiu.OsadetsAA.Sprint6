80c24bb58ef29e9b3655e28aa3c05c133149aa1e
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-12-10 23:40:58 +0500

    Task5

--- Tyuiu.OsadetsAA.Sprint6.Task5.V4.Test/DataServiceTest.cs
+++ Tyuiu.OsadetsAA.Sprint6.Task5.V4.Test/DataServiceTest.cs
@@ -10,7 +10,6 @@ namespace Tyuiu.OsadetsAA.Sprint6.Task5.V4.Test
             string[] paths = { @"C:\Users", "Mecht", "source", "repos", "Tyuiu.OsadetsAA.Sprint6", "Tyuiu.OsadetsAA.Sprint6.Task5.V4.Lib", "bin", "Debug", "InPutDataFileTask5V4.txt" };
             string fullPath = Path.Combine(paths);
 
-
             FileInfo fileInfo = new FileInfo(fullPath);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
